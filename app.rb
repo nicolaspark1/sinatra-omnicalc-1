fbe0326ccf2b075e6f1929de61ee174ce61fa80e
nicolaspark1 <141599069+nicolaspark1@users.noreply.github.com>
2024-10-22 23:48:37 +0000

    all but payment

--- app.rb
+++ app.rb
@@ -8,6 +8,7 @@ get("/") do
   "
 end
 
+#square 
 get("/square/new") do
  
   erb(:new_square_calc)
@@ -18,4 +19,32 @@ get("/square/results") do
 
   @the_result = @the_num ** 2
   erb(:square_results)
+end
+
+#square root
+
+get("/square_root/new") do
+ 
+  erb(:new_square_root_calc)
+end
+
+get("/square_root/results") do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = @the_num ** 0.5
+  erb(:square_root_results)
+end
+
+# random number
+get("/random/new") do
+ 
+  erb(:new_random_calc)
+end
+
+get("/random/results") do
+  @the_min = params.fetch("users_min").to_f
+  @the_max = params.fetch("users_max").to_f
+
+  @the_result = rand(@the_min..@the_max)
+  erb(:random_results)
 end
